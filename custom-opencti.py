76aaec473b04908ce01ddcb5c8d042782b3ced35
D3spere@ux <38745096+D3spereaux@users.noreply.github.com>
2025-05-14 16:36:48 +0700

    Re-modify def add_context and check dns in ids group.

--- custom-opencti.py
+++ custom-opencti.py
@@ -2,7 +2,7 @@
 
 # Copyright Andreas Misje 2024, 2022 Aurora Networks Managed Services
 # See https://github.com/misje/wazuh-opencti for documentation
-#
+# Modified by Brian Dao
 # This program is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
@@ -19,6 +19,7 @@ import json
 import ipaddress
 import re
 import traceback
+import logging
 
 # Maximum number of alerts to create for indicators found per query:
 max_ind_alerts = 3
@@ -38,30 +39,38 @@ sysmon_event3_regex = re.compile('sysmon_(?:event|eid)3')
 # Match sysmon_event_22 and sysmon_eid22_detections:
 sysmon_event22_regex = re.compile('sysmon_(?:event_|eid)22')
 # Location of source events file:
-log_file = '{0}/logs/integrations.log'.format(pwd)
+log_file = '/var/ossec/logs/debug-custom-opencti.log'
 # UNIX socket to send detections events to:
-socket_addr = '{0}/queue/sockets/queue'.format(pwd)
+socket_addr = '/var/ossec/queue/sockets/queue'
 # Find ";"-separated entries that are not prefixed with "type: X ". In order to
 # avoid non-fixed-width look-behind, match against the unwanted prefix, but
 # only group the match we care about, and filter out the empty strings later:
 dns_results_regex = re.compile(r'type:\s*\d+\s*[^;]+|([^\s;]+)')
 
+# Set up logging with debug level
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,  # Changed to DEBUG for detailed logging
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 def main(args):
     global url
-    debug('# Starting')
+    logger.debug('# Starting')
     alert_path = args[1]
     # Documentation says to do args[2].split(':')[1], but this is incorrect:
     token = args[2]
     url = args[3]
 
-    debug('# API key: {}'.format(token))
-    debug('# Alert file location: {}'.format(alert_path))
+#    logger.debug('# API key: {}'.format(token))
+#    logger.debug('# Alert file location: {}'.format(alert_path))
 
     with open(alert_path, errors='ignore') as alert_file:
         alert = json.load(alert_file)
 
-    debug('# Processing alert:')
-    debug(alert)
+    logger.debug('# Processing alert:')
+    logger.debug(alert)
 
     for new_alert in query_opencti(alert, url, token):
         send_event(new_alert, alert['agent'])
@@ -178,6 +187,20 @@ def indicator_link(indicator):
 # Modify the observable object so that it is more fit for opensearch (simplify
 # deeply-nested lists etc.):
 def modify_observable(observable, indicators):
+    """
+    Modify the observable object so that it is more fit for opensearch (simplify
+    deeply-nested lists etc.).
+
+    This function takes an observable object and a list of indicator objects as
+    input. It will generate a link to the observable, simplify the labels and
+    external references, grab the most relevant indicator (using the
+    indicator_sort_func), and generate a link to the indicator. Additionally, it
+    will indicate in the alert that there were multiple indicators.
+
+    :param observable: The observable object to modify
+    :param indicators: A list of indicator objects to pick from
+    :return: The modified observable object
+    """
     # Generate a link to the observable:
     observable['observable_link'] = url.removesuffix('graphql') + 'dashboard/observations/observables/{0}'.format(observable['id'])
 
@@ -208,6 +231,24 @@ def modify_observable(observable, indicators):
 # relationships to the observable, and if these objects have indicators, create
 # an alert:
 def relationship_with_indicators(node):
+    """
+    Analyzes the relationships of a given node to identify related indicators
+    and enriches them with additional information.
+
+    Parameters:
+    node (dict): A dictionary containing a node with its STIX core relationships.
+
+    Returns:
+    dict or None: A dictionary containing the most relevant related indicator
+    with its ID, type, relationship, value, a modified indicator object, and
+    a link to the indicator if available. Returns None if no such indicator is found.
+
+    The function processes the 'stixCoreRelationships' in the node, extracting
+    indicators from related nodes. It modifies these indicators to include
+    additional information relevant for alert generation. The indicators are
+    sorted based on relevance criteria, and the most relevant one is returned.
+    """
+
     related = []
     try:
         for relationship in node['stixCoreRelationships']['edges']:
@@ -229,52 +270,106 @@ def relationship_with_indicators(node):
     return next(iter(sorted(related, key=lambda x:indicator_sort_func(x['indicator']))), None)
 
 def add_context(source_event, event):
-    # Add source information to the original alert (naming convention
-    # from official VirusTotal integration):
-    event['opencti']['source'] = {}
-    event['opencti']['source']['alert_id'] = source_event['id']
-    event['opencti']['source']['rule_id'] = source_event['rule']['id']
-    if 'syscheck' in source_event:
-        event['opencti']['source']['file'] = source_event['syscheck']['path']
-        event['opencti']['source']['md5'] = source_event['syscheck']['md5_after']
-        event['opencti']['source']['sha1'] = source_event['syscheck']['sha1_after']
-        event['opencti']['source']['sha256'] = source_event['syscheck']['sha256_after']
-    if 'data' in source_event:
-        for key in ['in_iface', 'srcintf', 'src_ip', 'srcip', 'src_mac', 'srcmac', 'src_port', 'srcport', 'dest_ip', 'dstip', 'dst_mac', 'dstmac', 'dest_port', 'dstport', 'dstintf', 'proto', 'app_proto']:
-            if key in source_event['data']:
-                event['opencti']['source'][key] = source_event['data'][key]
-        if packetbeat_dns(source_event):
-            event['opencti']['source']['queryName'] = source_event['data']['dns']['question']['name']
-            if 'answers' in source_event['data']['dns']:
-                event['opencti']['source']['queryResults'] = ';'.join(map(lambda x:x['data'], source_event['data']['dns']['answers']))
-        if 'alert' in source_event['data']:
-            event['opencti']['source']['source_event'] = {}
-            for key in ['action', 'category', 'signature', 'signature_id']:
-                if key in source_event['data']['alert']:
-                    event['opencti']['source']['alert'][key] = source_event['data']['alert'][key]
-        if 'win' in source_event['data']:
-            if 'eventdata' in source_event['data']['win']:
+    """
+    Add context to an event based on a source event. The source event is
+    expected to be a JSON object with keys 'id', 'rule', 'syscheck', 'data',
+    and optionally 'alert' and 'win'. The function extracts relevant
+    information from the source event and adds it to the event as a nested
+    dictionary under the 'opencti' key.
+
+    The information extracted from the source event includes the alert_id and
+    rule_id, syscheck information (file, md5, sha1, sha256), data from the
+    source event (in_iface, srcintf, src_ip, srcip, src_mac, srcmac, src_port,
+    srcport, dest_ip, dstip, dest_mac, dstmac, dest_port, dstport, dstintf,
+    proto, app_proto), DNS data (queryName, queryResults), alert data
+    (action, category, signature, signature_id), Windows event data
+    (queryName, queryResults, image), and audit execve data (success, key,
+    uid, gid, euid, egid, exe, exit, pid).
+
+    If the source event does not contain the expected keys, the function will
+    not add any context to the event. If the source event contains invalid
+    data, the function will log a warning.
+
+    :param source_event: A JSON object containing the source event
+    :param event: The event to add context to
+    :return: The modified event with added context
+    """
+    logger.debug(f'Source Event: {source_event}')
+    logger.debug(f'Event: {event}')
+    try:
+        # Initialize opencti and source dictionaries if not present
+        if 'opencti' not in event:
+            event['opencti'] = {}
+        if 'source' not in event['opencti']:
+            event['opencti']['source'] = {}
+
+        # Add basic source information
+        event['opencti']['source']['alert_id'] = source_event['id']
+        event['opencti']['source']['rule_id'] = source_event['rule']['id']
+
+        # Add syscheck information if present
+        if 'syscheck' in source_event:
+            event['opencti']['source']['file'] = source_event['syscheck']['path']
+            event['opencti']['source']['md5'] = source_event['syscheck']['md5_after']
+            event['opencti']['source']['sha1'] = source_event['syscheck']['sha1_after']
+            event['opencti']['source']['sha256'] = source_event['syscheck']['sha256_after']
+
+        # Process data field if present
+        if 'data' in source_event:
+            for key in ['in_iface', 'srcintf', 'src_ip', 'srcip', 'src_mac', 'srcmac', 'src_port', 'srcport', 
+                        'dest_ip', 'dstip', 'dst_mac', 'dstmac', 'dest_port', 'dstport', 'dstintf', 'proto', 'app_proto']:
+                if key in source_event['data']:
+                    event['opencti']['source'][key] = source_event['data'][key]
+
+            # Process DNS data if present
+            if packetbeat_dns(source_event):
+                event['opencti']['source']['queryName'] = source_event['data']['dns']['question']['name']
+                if 'answers' in source_event['data']['dns']:
+                    event['opencti']['source']['queryResults'] = ';'.join(map(lambda x: x['data'], source_event['data']['dns']['answers']))
+
+            # Process alert data if present and valid
+            if 'alert' in source_event['data'] and isinstance(source_event['data']['alert'], dict):
+                event['opencti']['source']['alert'] = {}  # Initialize alert dictionary
+                for key in ['action', 'category', 'signature', 'signature_id']:
+                    if key in source_event['data']['alert']:
+                        event['opencti']['source']['alert'][key] = source_event['data']['alert'][key]
+                logger.debug("Added alert context for alert_id %s: %s", source_event['id'], event['opencti']['source']['alert'])
+            elif 'alert' in source_event['data']:
+                logger.warning("Invalid 'alert' data in source_event['data'] for alert_id %s: %s", 
+                               source_event['id'], source_event['data']['alert'])
+            else:
+                logger.debug("No 'alert' key in source_event['data'] for alert_id %s", source_event['id'])
+
+            # Process Windows event data if present
+            if 'win' in source_event['data'] and 'eventdata' in source_event['data']['win']:
                 for key in ['queryName', 'queryResults', 'image']:
                     if key in source_event['data']['win']['eventdata']:
                         event['opencti']['source'][key] = source_event['data']['win']['eventdata'][key]
-        if 'audit' in source_event['data'] and 'execve' in source_event['data']['audit']:
-            event['opencti']['source']['execve'] = ' '.join(source_event['data']['audit']['execve'][key] for key in sorted(source_event['data']['audit']['execve'].keys()))
-            for key in ['success', 'key', 'uid', 'gid', 'euid', 'egid', 'exe', 'exit', 'pid']:
-                if key in source_event['data']['audit']:
-                    event['opencti']['source'][key] = source_event['data']['audit'][key]
+
+            # Process audit execve data if present
+            if 'audit' in source_event['data'] and 'execve' in source_event['data']['audit']:
+                event['opencti']['source']['execve'] = ' '.join(source_event['data']['audit']['execve'][key] for key in sorted(source_event['data']['audit']['execve'].keys()))
+                for key in ['success', 'key', 'uid', 'gid', 'euid', 'egid', 'exe', 'exit', 'pid']:
+                    if key in source_event['data']['audit']:
+                        event['opencti']['source'][key] = source_event['data']['audit'][key]
+
+        logger.debug("Successfully added context for alert_id: %s", source_event['id'])
+    except Exception as e:
+        logger.error("Error adding context for alert_id %s: %s", source_event.get('id', 'unknown'), str(e))
 
 def send_event(msg, agent = None):
-    if not agent or agent['id'] == '000':
-        string = '1:opencti:{0}'.format(json.dumps(msg))
-    else:
-        string = '1:[{0}] ({1}) {2}->opencti:{3}'.format(agent['id'], agent['name'], agent['ip'] if 'ip' in agent else 'any', json.dumps(msg))
-
-    debug('# Event:')
-    debug(string)
-    sock = socket(AF_UNIX, SOCK_DGRAM)
-    sock.connect(socket_addr)
-    sock.send(string.encode())
-    sock.close()
+    """Send an event to the Wazuh Manager."""
+    try:
+        if not agent or agent['id'] == '000':
+            string = '1:opencti:{0}'.format(json.dumps(msg))
+        else:
+            string = '1:[{0}] ({1}) {2}->opencti:{3}'.format(agent['id'], agent['name'], agent['ip'] if 'ip' in agent else 'any', json.dumps(msg))
+        logger.debug(f"Sending Event: {string}")
+        with socket(AF_UNIX, SOCK_DGRAM) as sock:
+            sock.connect(socket_addr)
+            sock.send(string.encode())
+    except Exception as e:
+        logger.debug(f"Error sending event: {e}")
 
 def send_error_event(msg, agent = None):
     send_event({'integration': 'opencti', 'opencti': {
@@ -294,6 +389,25 @@ def oneof(*keys, within):
     return next((within[key] for key in keys if key in within), None)
 
 def query_opencti(alert, url, token):
+    """
+    Construct a query to the OpenCTI API and return a list of alerts based on the
+    response. The query is constructed based on the group names in the alert.
+    Currently, the following group names are processed:
+
+    - ids: Look up either dest or source IP, whichever is public
+    - sysmon_event3: Look up either dest or source IP, whichever is public
+    - sysmon_event22: Look up domain names in DNS queries, along with the results
+    - syscheck_file: Look up sha256 hashes for files added to the system or files
+      that have been modified
+    - osquery_file: Look up sha256 hashes in columns of any osqueries
+    - audit_command: Extract any command line arguments that looks vaguely like a
+      URL (starts with 'http')
+
+    :param alert: The alert to process
+    :param url: The URL of the OpenCTI API
+    :param token: The API token for the OpenCTI API
+    :return: A list of alerts based on the response from the OpenCTI API
+    """
     # The OpenCTI graphql query is filtering on a key and a list of values. By
     # default, this key is "value", unless set to "hashes.SHA256":
     filter_key='value'
@@ -326,6 +440,41 @@ def query_opencti(alert, url, token):
         # This may be tailored for suricata, but we'll match against the "ids"
         # group. These keys are probably used by other decoders as well:
         elif 'ids' in groups:
+            # Check if DNS data exists in the alert
+            if 'dns' in alert['data'] and 'query' in alert['data']['dns'] and alert['data']['dns']['query']:
+                # Extract rrname from DNS query
+                rrname = alert['data']['dns']['query'][0].get('rrname', '')
+                logger.debug(f'Extract rrname to check: {rrname}')
+                # Look up either dest or source IP, whichever is public
+                public_ip = next(filter(lambda x: x and ipaddress.ip_address(x).is_global, [
+                    oneof('dest_ip', 'dstip', within=alert['data']),
+                    oneof('src_ip', 'srcip', within=alert['data'])
+                ]), None)
+                filter_values = [public_ip, rrname] if public_ip else [rrname]
+                ind_filter = []
+                if public_ip:
+                    ind_filter.append(ind_ip_pattern(public_ip))
+                    logger.debug(f'Extract public IP to check: {public_ip}')
+                    logger.debug(f'New Indicator Filter: {ind_filter}')
+                if rrname:
+                    ind_filter.extend([
+                        f"[domain-name:value = '{rrname}']",
+                        f"[hostname:value = '{rrname}']"
+                    ])
+                    logger.debug(f'New Indicator Filter: {ind_filter}')
+            else:
+                # No DNS data, fall back to IP only
+                public_ip = next(filter(lambda x: x and ipaddress.ip_address(x).is_global, [
+                    oneof('dest_ip', 'dstip', within=alert['data']),
+                    oneof('src_ip', 'srcip', within=alert['data'])
+                ]), None)
+                filter_values = [public_ip]
+                ind_filter = [ind_ip_pattern(public_ip)] if public_ip else None
+                logger.debug(f'Extract public IP to check: {public_ip}')
+                logger.debug(f'New Indicator Filter: {ind_filter}')
+            if not all(v for v in filter_values if v):
+                sys.exit()
+
             # If data contains dns, it may contain a DNS query from packetbeat:
             if packetbeat_dns(alert):
                 addrs = filter_packetbeat_dns(alert['data']['dns']['answers']) if 'answers' in alert['data']['dns'] else []
@@ -337,6 +486,7 @@ def query_opencti(alert, url, token):
                 ind_filter = [ind_ip_pattern(filter_values[0])] if filter_values else None
             if not all(filter_values):
                 sys.exit()
+
         # Look up domain names in DNS queries (sysmon event 22), along with the
         # results (if they're IPv4/IPv6 addresses (A/AAAA records)):
         elif any(True for _ in filter(sysmon_event22_regex.match, groups)):
@@ -611,15 +761,15 @@ def query_opencti(alert, url, token):
                         ]
                     }
                     }}
-    #debug('# Query:')
-    #debug(api_json_body)
+    debug('# Query:')
+    debug(api_json_body)
 
     new_alerts = []
     try:
         response = requests.post(url, headers=query_headers, json=api_json_body)
     # Create an alert if the OpenCTI service cannot be reached:
     except ConnectionError:
-        log('Failed to connect to {}'.format(url))
+        logger.debug('Failed to connect to {}'.format(url))
         send_error_event('Failed to connect to the OpenCTI API', alert['agent'])
         sys.exit(1)
 
@@ -628,7 +778,7 @@ def query_opencti(alert, url, token):
     except json.decoder.JSONDecodeError:
         # If the API returns data, but not valid JSON, it is typically an error
         # code.
-        log('# Failed to parse response from API')
+        logger.debug('# Failed to parse response from API')
         send_error_event('Failed to parse response from OpenCTI API', alert['agent'])
         sys.exit(1)
 
@@ -674,7 +824,7 @@ def query_opencti(alert, url, token):
         # If the observable has no indicators, ignore it:
         if not indicators and not related_obs_w_ind:
             # TODO: Create event for this?
-            debug(f'# Observable found ({node["id"]}), but it has no indicators')
+            logger.debug(f'# Observable found ({node["id"]}), but it has no indicators')
             continue
 
         new_alert = {'integration': 'opencti', 'opencti': edge['node']}
@@ -695,8 +845,9 @@ if __name__ == '__main__':
     try:
         if len(sys.argv) >= 4:
             debug('{0} {1} {2} {3}'.format(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4] if len(sys.argv) > 4 else ''), do_log = True)
+            logger.debug('{0} {1} {2} {3}'.format(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4] if len(sys.argv) > 4 else ''))
         else:
-            log('Incorrect arguments: {0}'.format(' '.join(sys.argv)))
+            logger.debug('Incorrect arguments: {0}'.format(' '.join(sys.argv)))
             sys.exit(1)
 
         debug_enabled = len(sys.argv) > 4 and sys.argv[4] == 'debug'
